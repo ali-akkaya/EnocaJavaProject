5ef2b1c9a8a919547e269edff5f97b7ea71c03ce
aliakkaya <aa.aliakkaya@gmail.com>
2023-07-30 16:46:10 +0300

    Initial commit with dependencies

---
+++ src/main/java/com/example/enocajavaproject/EnocaJavaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.enocajavaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnocaJavaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnocaJavaProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/enocajavaproject/EnocaJavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enocajavaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnocaJavaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
