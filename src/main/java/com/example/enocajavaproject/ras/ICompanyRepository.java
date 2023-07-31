7db82e065e4f1e0ab8b9dfb62ef2ecae323f3d2b
aliakkaya <aa.aliakkaya@gmail.com>
2023-07-31 22:32:48 +0300

    Add repositories

--- src/main/java/com/example/enocajavaproject/ras/ICompanyRepository.java
+++ src/main/java/com/example/enocajavaproject/ras/ICompanyRepository.java
@@ -1,7 +1,11 @@
 package com.example.enocajavaproject.ras;
 
-import com.example.enocajavaproject.dto.Company;
+import com.example.enocajavaproject.model.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ICompanyRepository extends JpaRepository<Company, Integer> {
+
+    Optional<Company> findByCompanyName(String companyName);
 }
--- src/main/java/com/example/enocajavaproject/ras/IEmployeeRepository.java
+++ src/main/java/com/example/enocajavaproject/ras/IEmployeeRepository.java
@@ -1,7 +1,11 @@
 package com.example.enocajavaproject.ras;
 
-import com.example.enocajavaproject.dto.Employee;
+import com.example.enocajavaproject.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IEmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    List<Employee> findAllByCompany_Id(Integer companyId);
 }
